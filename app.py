2e4515680b6f86a9bfa1b1ea743c6d2cb61ea9e2
rileythejones <rileymjones@gmail.com>
2020-01-08 15:51:21 -0700

    basic functionality

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 import pandas as pd
 from flask import Flask, jsonify, request
-import pickle
+import json
 
-# load model
-model = pickle.load(open('model.pkl','rb'))
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import NearestNeighbors
+
+
+df = pd.read_csv('SpotifyAudioFeaturesApril2019.csv')
+
+# Separate data into features and targets
+# target is the first 3 columns, ie. artist, song_id, and song name
+target = df.columns[:3] 
+features = df.columns[3:]
+
+X = df[features]
+y = df[target]
+
+# remove potentially unecessary columns
+drop_cols = ['duration_ms', 'key', 'mode', 'time_signature', 'popularity','tempo'] 
+X = X.drop(columns=drop_cols)
+
+# scaling 
+scaler = MinMaxScaler()
+scaler.fit_transform(X)
+
+# model 
+model = NearestNeighbors(n_neighbors=10, algorithm='kd_tree')
+model.fit(X)
 
 # app
 app = Flask(__name__)
@@ -15,15 +38,15 @@ def predict():
     # get data
     data = request.get_json(force=True)
 
-    # convert data into dataframe
-    data.update((x, [y]) for x, y in data.items())
-    data_df = pd.DataFrame.from_dict(data)
-
+    # convert data into series # try json.loads else
+    labels = ['artist_name', 'track_id', 'track_name', 'duration_ms', 'key', 'mode', 'tempo', 'time_signature', 'popularity']      
+    predictor = pd.Series(json.loads(data)).drop(labels=labels)
+    
     # predictions
-    result = model.predict(data_df)
-
+    recommendations = model.kneighbors([predictor])[1][0]
+    
     # send back to browser
-    output = {'results': int(result[0])}
+    output = {'results': str(y.iloc[recommendations][:2])}
 
     # return data
     return jsonify(results=output)
